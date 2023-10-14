dcc43c17bb96e2d164f10a9f380c272d2ca85415
muhammad.aichee <ahsan.siddiq@astera.com>
2023-10-14 13:54:03 +0500

    User validator added

--- validators/UserValidators.js
+++ validators/UserValidators.js
@@ -2,10 +2,11 @@ const { check } = require('express-validator');
 
 const validateUser = [
     check('username').notEmpty().withMessage('Username is required'),
+    check('email').notEmpty().withMessage('Email is required'),
     check('email').isEmail().withMessage('Invalid email format'),
     check('country').notEmpty().withMessage('Country is required'),
-    check('state').notEmpty().withMessage('State is required'),
-    check('city').notEmpty().withMessage('City is required'),
+    // check('state').notEmpty().withMessage('State is required'),
+    // check('city').notEmpty().withMessage('City is required'), 
   ];
 
 module.exports = validateUser;  
