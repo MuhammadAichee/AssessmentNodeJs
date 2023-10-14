e8f265e8563428d5fd412de334e0b387228706c6
muhammad.aichee <ahsan.siddiq@astera.com>
2023-10-14 15:05:52 +0500

    Added put and delete api

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,9 +3,11 @@ const userRouter = express.Router();
 const { validationResult } = require("express-validator");
 const User = require("../models/User");
 const Country = require("../models/Country");
+const City = require("../models/City");
 const UserValidator = require("../validators/UserValidators");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { ObjectId } = require("mongodb");
 const authenticateUser = require("../middleware/authenticationMiddleware");
 
 userRouter.get("/", async (req, res) => {
@@ -64,8 +66,48 @@ userRouter.post("/login", async (req, res) => {
 });
 
 userRouter.get("/check", authenticateUser, async (req, res) => {
-  res
-  .status(200)
-  .send({ Message: "Authenticate" });
+  res.status(200).send({ Message: "Authenticate" });
 });
+
+userRouter.put("/:id", authenticateUser, UserValidator, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  if (!req.params.id)
+    return res.status(400).json({ errors: "Please provide user ID" });
+  const { email, country, city } = req.body;
+  const user = await User.findById(new ObjectId(req.params.id));
+  if (!user) {
+    return res.status(401).json({ errors: "User not found" });
+  }
+
+  const findCountry = await Country.find({ name: country });
+  if (!findCountry) {
+    return res.status(401).json({ errors: "Country not found" });
+  }
+  const findCity = await City.find({ name: city, country: findCountry._id });
+  if (!findCity) {
+    return res
+      .status(401)
+      .json({ errors: "City not found related to provided country" });
+  }
+  const updatedUser = await User.findByIdAndUpdate(
+    user._id,
+    { country: findCountry._id, city: findCity._id, email: email },
+    { new: true }
+  );
+  return res.status(200).json(updatedUser);
+});
+
+userRouter.delete("/:id", async (req, res) => {
+  if (!req.params.id)
+    return res.status(400).json({ errors: "Please provide user ID" });
+  const deletedUser = await User.findByIdAndDelete(new ObjectId(req.params.id));
+  if (!deletedUser) {
+    return res.status(401).json({ errors: "Usser not found" });
+  }
+  return res.status(200).json({ "Deleted User": deletedUser });
+});
+
 module.exports = userRouter;
